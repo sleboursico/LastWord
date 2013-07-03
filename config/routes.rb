c67c9ae5f80bdf424d962a3b7a337aa3f4d972f5
Seb <sebastien.leboursico@gmail.com>
2013-07-03 18:10:20 +0200

    Fisrt commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails3BootstrapDeviseCancan::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users
+end
